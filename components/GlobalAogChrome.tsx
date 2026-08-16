51fb6c5c8e35ca926976751eed223c0e0ebb2eca
Hakan Ozcilingir <hakannex@gmail.com>
2026-08-15 23:14:11 -0400

    Build feature-gated Price Check journey

--- components/GlobalAogChrome.tsx
+++ components/GlobalAogChrome.tsx
@@ -1,4 +1,4 @@
 "use client";
 import { useSyncExternalStore } from "react"; import { AogContactStrip } from "./AogContactStrip"; import { MobileAogBar } from "./MobileAogBar";
 const subscribe=()=>()=>{};const getPath=()=>window.location.pathname;const getServerPath=()=>"/";
-export function GlobalAogChrome(){const path=useSyncExternalStore(subscribe,getPath,getServerPath),disableMobileBar=path==="/aog-services"||path==="/contact-us";return <><AogContactStrip sourcePage={path}/>{!disableMobileBar&&<MobileAogBar sourcePage={path}/>}</>}
+export function GlobalAogChrome(){const path=useSyncExternalStore(subscribe,getPath,getServerPath),disableMobileBar=path==="/aog-services"||path==="/contact-us"||path==="/price-check";return <><AogContactStrip sourcePage={path}/>{!disableMobileBar&&<MobileAogBar sourcePage={path}/>}</>}
